91843c8cd76c7f6095230966f2340ab915e68a60
uma-ram <jananijishnumar@gmail.com>
2026-01-09 12:20:48 +0000

    Added TodoApi.Test project

---
+++ TodoApi.Tests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApi.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        //Arrange (setup)
+        int a = 7;
+        int b = 7;
+
+        //Act (execute)
+        int result = a + b;
+
+        //Assert (verify)
+        Assert.Equal(14, result);
+    }
+}
